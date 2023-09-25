f20ba47f71e874638714546f36bfecee1e9979de
ArchimedesTW <f.andryukhin@yandex.ru>
2023-09-25 16:38:52 +0300

    new class

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
 import socket
 from socket import socket as sc
 import time
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
 
+engine = create_engine("postgresql+psycopg2://postgres:Nikusha1001@localhost/rebotica")
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+s = Session()
 main_socket = sc(socket.AF_INET, socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 main_socket.bind(("localhost", 1000))
@@ -9,31 +18,80 @@ main_socket.setblocking(False)
 main_socket.listen(5)
 print("Сокет создался")
 
-players = []
+
+class Player(Base):
+    __tablename__ = "gamers"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(250))
+    address = Column(String)
+    x = Column(Integer, default=500)
+    y = Column(Integer, default=500)
+    size = Column(Integer, default=50)
+    errors = Column(Integer, default=0)
+    abs_speed = Column(Integer, default=1)
+    speed_x = Column(Integer, default=0)
+    speed_y = Column(Integer, default=0)
+
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+
+Base.metadata.create_all(engine)
+
+
+class LocalPlayer:
+    def __init__(self, id, name, sock, addr):
+        self.id = id
+        self.db: Player = s.get(Player, self.id)
+        self.sock = sock
+        self.name = name
+        self.address = addr
+        self.x = 500
+        self.y = 500
+        self.size = 50
+        self.errors = 0
+        self.abs_speed = 1
+        self.speed_x = 0
+        self.speed_y = 0
+
+
+players = {}
 while True:
     # подключаемся
     try:
         new_socket, addr = main_socket.accept()
         print('Подключился', addr)
         new_socket.setblocking(False)
-        players.append(new_socket)
+        player = Player("name", addr)
+        s.merge(player)
+        s.commit()
+        addr = f"({addr[0]},{addr[1]})"
+        data = s.query(Player).filter(Player.address == addr)
+        for user in data:
+            player = LocalPlayer(user.id, "Имя", new_socket, addr)
+            players[user.id] = player
+        # players.append(new_socket)
 
     except BlockingIOError:
         pass
-    for sock in players:
+
+    for id in list(players):
         try:
-            data = sock.recv(1024).decode()
+            data = players[id].sock.recv(1024).decode()
             print("Получил", data)
         except:
             pass
 
     # Отправка игрокам поля
-    for sock in players:
+    for id in list(players):
         try:
-            sock.send("LOL".encode())
+            players[id].sock.send("Игра".encode())
         except:
-            players.remove(sock)
-            sock.close()
+            players[id].sock.close()
+            del players[id]
+            s.query(Player).filter(Player.id == id).delete()
+            s.commit()
             print("Сокет закрыт")
 
     time.sleep(1)
